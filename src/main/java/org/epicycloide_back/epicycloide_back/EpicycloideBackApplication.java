6de538473d526644491e7d9dc59f255a50588cd5
theophile <theophile.gillier@gmail.com>
2024-05-06 13:06:22 +0200

    init project

---
+++ src/main/java/org/epicycloide_back/epicycloide_back/EpicycloideBackApplication.java
@@ -0,0 +1,13 @@
+package org.epicycloide_back.epicycloide_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicycloideBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpicycloideBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/epicycloide_back/epicycloide_back/EpicycloideBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.epicycloide_back.epicycloide_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicycloideBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
