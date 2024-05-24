4f81b84e7c0befb4c05c5d9e2418c8721cdbed28
theophile <theophile.gillier@gmail.com>
2024-05-24 17:25:37 +0200

    mouai

--- src/main/java/org/epicycloide_back/epicycloide_back/model/Epicycloid.java
+++ src/main/java/org/epicycloide_back/epicycloide_back/model/Epicycloid.java
@@ -3,15 +3,16 @@ package org.epicycloide_back.epicycloide_back.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
+import org.apache.commons.math3.exception.MathIllegalArgumentException;
 import org.apache.commons.math3.transform.DftNormalization;
 import org.apache.commons.math3.transform.FastFourierTransformer;
 import org.apache.commons.math3.complex.Complex;
 import org.apache.commons.math3.transform.TransformType;
+import org.apache.commons.math3.util.ArithmeticUtils;
 import org.epicycloide_back.epicycloide_back.validation.GreaterThan;
 import org.epicycloide_back.epicycloide_back.util.FractionConverter;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 @Entity
 public class Epicycloid {
@@ -173,59 +174,66 @@ public class Epicycloid {
 
     public static Epicycloid transform(ArrayList<Point> points, int precision) {
 
-        double[][] data = new double[points.size()][2];
-        int index = 0;
-        for (Point point : points) {
-            data[index++] = new double[]{point.getX(), point.getY()};
-        }
-
-        double[] flatPoints = new double[data.length * 2];
-        int index2 = 0;
-        for (double[] point : data) {
-            flatPoints[index2++] = point[0];
-            flatPoints[index2++] = point[1];
-        }
-
-        int newSize = 1 << (int) (Math.ceil(Math.log10(flatPoints.length) / Math.log10(2)));
-        if (newSize > flatPoints.length) {
-            flatPoints = Arrays.copyOf(flatPoints, newSize);
-        }
-
         FastFourierTransformer fft = new FastFourierTransformer(DftNormalization.STANDARD);
 
-        Complex[] fftResult = fft.transform(flatPoints, TransformType.FORWARD);
+        Complex[] pointsArray = points.stream()
+                .map(point -> new Complex(point.getX(), point.getY()))
+                .toArray(Complex[]::new);
 
-        for (Complex result : fftResult) {
-            System.out.println("Re: " + result.getReal() + ", Im: " + result.getImaginary());
-        }
+        Complex[] fftResult = new Complex[0];
 
-        // Calcul de la fréquence d'échantillonnage
-        double fs = precision; // Exemple de fréquence d'échantillonnage
+        try {
+            fftResult = fft.transform(pointsArray, TransformType.FORWARD);
 
-        Epicycloid epicycloid = new Epicycloid();
-        Epicycloid rolling = null;
+//        System.out.println(fftResult.length);
 
-        // Affichage des résultats
-        for (int k = 0; k < precision; ++k) {
-            double freq = k * fs / fftResult.length;
-            double mag = fftResult[k].abs(); // Amplitude
-            double phase = fftResult[k].getArgument(); // Phase en radians
+//        for (Complex result : fftResult) {
+//            System.out.println("Re: " + result.getReal() + ", Im: " + result.getImaginary());
+//        }
 
-            System.out.printf("Fréquence: %.2f Hz, Amplitude: %.2f, Phase: %.2f rad\n", freq, mag, phase);
+            System.out.println("Re: " + fftResult[0].getReal() + ", Im: " + fftResult[0].getImaginary());
+            System.out.println("Re: " + fftResult[1].getReal() + ", Im: " + fftResult[1].getImaginary());
+            System.out.println("Re: " + fftResult[2].getReal() + ", Im: " + fftResult[2].getImaginary());
+            System.out.println("Re: " + fftResult[3].getReal() + ", Im: " + fftResult[3].getImaginary());
+            System.out.println("Re: " + fftResult[4].getReal() + ", Im: " + fftResult[4].getImaginary());
+            System.out.println("Re: " + fftResult[5].getReal() + ", Im: " + fftResult[5].getImaginary());
+            System.out.println("Re: " + fftResult[6].getReal() + ", Im: " + fftResult[6].getImaginary());
+            System.out.println("Re: " + fftResult[7].getReal() + ", Im: " + fftResult[7].getImaginary());
 
-            Epicycloid newEpicycloid = new Epicycloid(freq, mag, phase);
 
-            if (k == 0) {
-                epicycloid = newEpicycloid;
-                rolling = epicycloid;
-            } else {
-                rolling.setRolling(newEpicycloid);
-                rolling = newEpicycloid;
+            Epicycloid epicycloid = new Epicycloid();
+            Epicycloid rolling = null;
+
+            int maxLength = Math.min(precision * 2, fftResult.length);
+
+            // Affichage des résultats
+            for (int k = 1; k < maxLength; ++k) {
+                double freq = (k + 1) % 2 == 0 ? (k / 2) + 1 : -(k + 1) / 2;
+                double radius = Math.sqrt(
+                        Math.pow(fftResult[k].getReal(), 2) + Math.pow(fftResult[k].getImaginary(), 2)
+                );
+
+                double phase = fftResult[k].getArgument();
+
+//            System.out.printf("Fréquence: %.2f, Amplitude: %.2f, Phase: %.2f°\n", freq, radius, phase);
+
+                Epicycloid newEpicycloid = new Epicycloid(freq, radius, phase);
+
+                if (k == 1) {
+                    epicycloid = newEpicycloid;
+                    rolling = epicycloid;
+                } else {
+                    rolling.setRolling(newEpicycloid);
+                    rolling = newEpicycloid;
+                }
             }
+
+            return epicycloid;
+
+        } catch (MathIllegalArgumentException exception) {
+            points.add(new Point(0, 0, 0));
+            return transform(points, precision);
         }
-
-
-        return epicycloid;
     }
 
     @Override
